7a5fe23ee61be9915a23a570be921cf1259fa512
Jai <jsarava2@ncsu.edu>
2024-12-09 18:52:53 -0500

    Revert "bro"
    This reverts commit 106d9ab806c6a8e4191791ae2aabfc58ea08d5d0.

--- benefits-app/src/components/HomePage/HomePage.js
+++ benefits-app/src/components/HomePage/HomePage.js
@@ -3,6 +3,10 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 import QuestionInput from '../UserForm/QuestionInput';
 
+const states = [
+  "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
+];
+
 function HomePage() {
   const [commonQuiz, setCommonQuiz] = useState(null);
   const [questions, setQuestions] = useState([]);
