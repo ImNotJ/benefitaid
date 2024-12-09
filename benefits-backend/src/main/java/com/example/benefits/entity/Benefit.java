3e6fb57b63910e2b2ed8670b7a38827300ff6d49
Jai <jsarava2@ncsu.edu>
2024-12-09 18:53:53 -0500

    Revert "test again"
    This reverts commit 8cc6f0b130bf62fadf2a0215cd7541afa691246f.

--- benefits-backend/src/main/java/com/example/benefits/entity/Benefit.java
+++ benefits-backend/src/main/java/com/example/benefits/entity/Benefit.java
@@ -1,6 +1,7 @@
 package com.example.benefits.entity;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.util.Set;
@@ -34,69 +35,136 @@ public class Benefit {
     
     @Column
     private String imageUrl;
+    
+    // Add getters and setters
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public String getImageUrl() {
+        return imageUrl;
+    }
+    
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 
-    // Getters and setters
+    // Getters and Setters
+
+    /**
+     * Gets the ID of the benefit.
+     *
+     * @return the ID of the benefit
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Sets the ID of the benefit.
+     *
+     * @param id the ID to set
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Gets the name of the benefit.
+     *
+     * @return the name of the benefit
+     */
     public String getBenefitName() {
         return benefitName;
     }
 
+    /**
+     * Sets the name of the benefit.
+     *
+     * @param benefitName the name to set
+     */
     public void setBenefitName(String benefitName) {
         this.benefitName = benefitName;
     }
 
+    /**
+     * Checks if the benefit is federal.
+     *
+     * @return true if the benefit is federal, false otherwise
+     */
     public boolean isFederal() {
         return federal;
     }
 
+    /**
+     * Sets whether the benefit is federal.
+     *
+     * @param federal the federal status to set
+     */
     public void setFederal(boolean federal) {
         this.federal = federal;
+        if (federal) {
+            this.state = null; // Clear state if federal is true
+        }
     }
 
+    /**
+     * Gets the state associated with the benefit.
+     *
+     * @return the state associated with the benefit
+     */
     public String getState() {
         return state;
     }
 
+    /**
+     * Sets the state associated with the benefit.
+     *
+     * @param state the state to set
+     */
     public void setState(String state) {
-        this.state = state;
+        if (!this.federal) {
+            this.state = state;
+        }
     }
 
+    /**
+     * Gets the URL of the benefit.
+     *
+     * @return the URL of the benefit
+     */
     public String getBenefitUrl() {
         return benefitUrl;
     }
 
+    /**
+     * Sets the URL of the benefit.
+     *
+     * @param benefitUrl the URL to set
+     */
     public void setBenefitUrl(String benefitUrl) {
         this.benefitUrl = benefitUrl;
     }
 
+    /**
+     * Gets the requirements associated with the benefit.
+     *
+     * @return the requirements associated with the benefit
+     */
     public Set<Requirement> getRequirements() {
         return requirements;
     }
 
+    /**
+     * Sets the requirements associated with the benefit.
+     *
+     * @param requirements the requirements to set
+     */
     public void setRequirements(Set<Requirement> requirements) {
         this.requirements = requirements;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
     }
 }
--- benefits-backend/src/main/java/com/example/benefits/entity/Requirement.java
+++ benefits-backend/src/main/java/com/example/benefits/entity/Requirement.java
@@ -1,6 +1,7 @@
 package com.example.benefits.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.util.Set;
@@ -38,21 +39,78 @@ public class Requirement {
         GENERAL_NECESSARY
     }
 
-    // Getters and setters
+    // Getters and Setters
+
+    /**
+     * Gets the ID of the requirement.
+     *
+     * @return the ID of the requirement
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Sets the ID of the requirement.
+     *
+     * @param id the ID to set
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Gets the name of the requirement.
+     *
+     * @return the name of the requirement
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the requirement.
+     *
+     * @param name the name to set
+     */
     public void setName(String name) {
         this.name = name;
+    }
+
+    /**
+     * Gets the set of conditions associated with the requirement.
+     *
+     * @return the set of conditions
+     */
+    public Set<Condition> getConditions() {
+        return conditions;
+    }
+
+    /**
+     * Sets the set of conditions associated with the requirement.
+     *
+     * @param conditions the set of conditions to set
+     */
+    public void setConditions(Set<Condition> conditions) {
+        this.conditions = conditions;
+    }
+
+    /**
+     * Gets the benefit associated with the requirement.
+     *
+     * @return the benefit associated with the requirement
+     */
+    public Benefit getBenefit() {
+        return benefit;
+    }
+
+    /**
+     * Sets the benefit associated with the requirement.
+     *
+     * @param benefit the benefit to set
+     */
+    public void setBenefit(Benefit benefit) {
+        this.benefit = benefit;
     }
 
     public RequirementType getType() {
@@ -61,21 +119,5 @@ public class Requirement {
 
     public void setType(RequirementType type) {
         this.type = type;
-    }
-
-    public Set<Condition> getConditions() {
-        return conditions;
-    }
-
-    public void setConditions(Set<Condition> conditions) {
-        this.conditions = conditions;
-    }
-
-    public Benefit getBenefit() {
-        return benefit;
-    }
-
-    public void setBenefit(Benefit benefit) {
-        this.benefit = benefit;
     }
 }
