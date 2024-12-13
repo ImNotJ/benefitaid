86d3c52292a78646c3b6a4751588264ea4495d56
Jai <jsarava2@ncsu.edu>
2024-12-13 15:48:58 -0500

    Revert "Trial Manage Questions"
    This reverts commit ae6a0cd0f790719ae8ebeaf6fa84dd61ac5bf380.

--- benefits-backend/src/main/java/com/example/benefits/controller/QuestionController.java
+++ benefits-backend/src/main/java/com/example/benefits/controller/QuestionController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+/**
+ * REST controller for managing Question entities.
+ */
 @RestController
 @RequestMapping("/api/questions")
 public class QuestionController {
@@ -15,27 +18,56 @@ public class QuestionController {
     @Autowired
     private QuestionService questionService;
 
+    /**
+     * Endpoint to create a new question.
+     *
+     * @param question the question entity to create
+     * @return the created question entity
+     */
     @PostMapping
     public Question createQuestion(@Valid @RequestBody Question question) {
         return questionService.saveQuestion(question);
     }
 
+    /**
+     * Endpoint to get a question by ID.
+     *
+     * @param id the ID of the question
+     * @return the question entity
+     */
     @GetMapping("/{id}")
     public Question getQuestionById(@PathVariable Long id) {
         return questionService.getQuestionById(id);
     }
 
+    /**
+     * Endpoint to get all questions.
+     *
+     * @return a list of all question entities
+     */
     @GetMapping
     public List<Question> getAllQuestions() {
         return questionService.getAllQuestions();
     }
 
+    /**
+     * Endpoint to update a question.
+     *
+     * @param id       the ID of the question to update
+     * @param question the updated question entity
+     * @return the updated question entity
+     */
     @PutMapping("/{id}")
     public Question updateQuestion(@PathVariable Long id, @Valid @RequestBody Question question) {
         question.setId(id);
         return questionService.saveQuestion(question);
     }
 
+    /**
+     * Endpoint to delete a question by ID.
+     *
+     * @param id the ID of the question to delete
+     */
     @DeleteMapping("/{id}")
     public void deleteQuestion(@PathVariable Long id) {
         questionService.deleteQuestionById(id);
--- benefits-backend/src/main/java/com/example/benefits/entity/Question.java
+++ benefits-backend/src/main/java/com/example/benefits/entity/Question.java
@@ -1,9 +1,21 @@
 package com.example.benefits.entity;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.data.annotation.Transient;
+
+/**
+ * Entity class representing a Question.
+ */
 @Entity
 public class Question {
 
@@ -15,53 +27,102 @@ public class Question {
     private String questionName;
 
     @NotBlank
-    private String questionType;
+    private String questionType; // Text, Numerical, Date, Email, MultiChoiceSingle, MultiChoiceMulti
 
     @NotBlank
     private String questionText;
-
-    @Transient
-    private List<String> optionsList;
+    
+    @Column(columnDefinition = "TEXT")
+    private String options;
 
     // Getters and Setters
 
+    /**
+     * Gets the ID of the question.
+     *
+     * @return the ID of the question
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Sets the ID of the question.
+     *
+     * @param id the ID to set
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Gets the name of the question.
+     *
+     * @return the name of the question
+     */
     public String getQuestionName() {
         return questionName;
     }
 
+    /**
+     * Sets the name of the question.
+     *
+     * @param questionName the name to set
+     */
     public void setQuestionName(String questionName) {
         this.questionName = questionName;
     }
 
+    /**
+     * Gets the type of the question.
+     *
+     * @return the type of the question
+     */
     public String getQuestionType() {
         return questionType;
     }
 
+    /**
+     * Sets the type of the question.
+     *
+     * @param questionType the type to set
+     */
     public void setQuestionType(String questionType) {
         this.questionType = questionType;
     }
 
+    /**
+     * Gets the text of the question.
+     *
+     * @return the text of the question
+     */
     public String getQuestionText() {
         return questionText;
     }
 
+    /**
+     * Sets the text of the question.
+     *
+     * @param questionText the text to set
+     */
     public void setQuestionText(String questionText) {
         this.questionText = questionText;
     }
 
-    public List<String> getOptionsList() {
-        return optionsList;
+    public String getOptions() {
+        return options;
     }
 
-    public void setOptionsList(List<String> optionsList) {
-        this.optionsList = optionsList;
+    public void setOptions(String options) {
+        this.options = options;
+    }
+
+    // Helper method to get options as list
+    @Transient
+    public List<String> getOptionsList() {
+        if (options == null || options.trim().isEmpty()) {
+            return new ArrayList<>();
+        }
+        return Arrays.asList(options.split(","));
     }
 }
