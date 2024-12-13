4bc450b869faebcb1b557423faa0479607008197
Jai <jsarava2@ncsu.edu>
2024-12-13 15:52:57 -0500

    Reapply "backend eddits to question controller"
    This reverts commit f3a11363131588eb00cea047087a3a45197789ca.

--- benefits-backend/src/main/java/com/example/benefits/service/QuestionService.java
+++ benefits-backend/src/main/java/com/example/benefits/service/QuestionService.java
@@ -16,41 +16,19 @@ public class QuestionService {
     @Autowired
     private QuestionRepository questionRepository;
 
-    /**
-     * Saves a question entity.
-     *
-     * @param question the question entity to save
-     * @return the saved question entity
-     */
     public Question saveQuestion(Question question) {
         // Save question and options
         return questionRepository.save(question);
     }
 
-    /**
-     * Gets a question by ID.
-     *
-     * @param id the ID of the question
-     * @return the question entity, or null if not found
-     */
     public Question getQuestionById(Long id) {
         return questionRepository.findById(id).orElse(null);
     }
 
-    /**
-     * Gets all questions.
-     *
-     * @return a list of all question entities
-     */
     public List<Question> getAllQuestions() {
         return questionRepository.findAll();
     }
 
-    /**
-     * Deletes a question by ID.
-     *
-     * @param id the ID of the question to delete
-     */
     public void deleteQuestionById(Long id) {
         questionRepository.deleteById(id);
     }
