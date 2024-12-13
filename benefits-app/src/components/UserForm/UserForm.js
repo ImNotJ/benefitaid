92d93ccac437e9dcf8875c68ce203db7c1e4d355
Jai <jsarava2@ncsu.edu>
2024-12-13 15:49:39 -0500

    Revert "Reapply "Reapply "test"""
    This reverts commit 53f638253706928b57a7d34a6961d7422d92510a.

--- benefits-app/src/components/UserForm/UserForm.js
+++ benefits-app/src/components/UserForm/UserForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from '../../utils/axiosConfig';
 import QuestionInput from './QuestionInput';
 import { isValidEmail, formatDate } from '../../utils/validation';
