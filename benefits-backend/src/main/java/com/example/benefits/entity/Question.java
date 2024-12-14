eec602069d1f3e0c425ad19761a0ed73c6b7544f
Jai <jsarava2@ncsu.edu>
2024-12-14 17:03:01 -0500

    Trial new options

--- benefits-backend/src/main/java/com/example/benefits/entity/Question.java
+++ benefits-backend/src/main/java/com/example/benefits/entity/Question.java
@@ -2,6 +2,7 @@ package com.example.benefits.entity;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import com.example.benefits.converter.StringListConverter;
 import java.util.List;
 
 @Entity
@@ -20,9 +21,8 @@ public class Question {
     @NotBlank
     private String questionText;
 
-    @ElementCollection
-    @CollectionTable(name = "question_options", joinColumns = @JoinColumn(name = "question_id"))
-    @Column(name = "option_value")
+    @Column(columnDefinition = "json")
+    @Convert(converter = StringListConverter.class)
     private List<String> options; // Store options as a list of strings
 
     // Getters and Setters
