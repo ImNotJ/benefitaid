83ccaabee5dd4ac92d548fa798d177b359a05446
Jai <jsarava2@ncsu.edu>
2024-12-18 12:28:24 -0500

    Revert "bro"
    This reverts commit a5d3e8cb0fb6beb670ae0bc583092715452f814f.

--- benefits-app/src/components/ManageQuestions/ManageQuestions.js
+++ benefits-app/src/components/ManageQuestions/ManageQuestions.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios from '../../utils/axiosConfig';
+import axios from 'axios';
 import './ManageQuestions.css';
 
 function ManageQuestions() {
